4686d4e74a8380fbebf6b2efb9359889b5eee44a
EC2 Default User <ec2-user@ip-172-31-21-94.ap-northeast-1.compute.internal>
2022-03-06 02:58:12 +0000

    createアクションのコントローラーを定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,14 @@ class TweetsController < ApplicationController
     def new
         @tweet = Tweet.new #form_withから送られてきた情報が＠tweet（インスタンス）を作ってcreateアクションに渡される
     end
+    
+    def create(tweet_params)
+    end 
+    
+    private
+    
+    def tweet_params
+        params.require(:Tweet).parmit(:mane, :image, :text)
+    end 
+    
 end
