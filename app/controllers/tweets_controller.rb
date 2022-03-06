b783b4431b60c2816b6e35342c4bd3dbc88de2ac
EC2 Default User <ec2-user@ip-172-31-21-94.ap-northeast-1.compute.internal>
2022-03-06 02:38:00 +0000

    newアクションのビューの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,10 @@
 class TweetsController < ApplicationController
+    
     def index
         @tweets = Tweet.all
     end
     
     def new
-        @tweet = Tweet.new
+        @tweet = Tweet.new #form_withから送られてきた情報が＠tweet（インスタンス）を作ってcreateアクションに渡される
+    end
 end
