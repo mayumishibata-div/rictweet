3aa6dc5403c7416f90c57aa7b984063ce475c5de
EC2 Default User <ec2-user@ip-172-31-21-94.ap-northeast-1.compute.internal>
2022-03-09 12:09:08 +0000

    ユーザーマイページのビューへ渡すインスタンス変数を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
     def show
-        @nickname = current_user.nickname
-        @tweets = current_user.tweets
+        user = User.find(params[:id])
+        @nickname = user.nickname
+        @tweets = user.tweets
+        # @nickname = current_user.nickname
+        # @tweets = current_user.tweets
     end
 end
 
